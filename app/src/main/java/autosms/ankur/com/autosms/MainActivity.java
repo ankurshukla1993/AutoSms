66d81b6e814f082572423b6554c24239e60bfd3b
xxx <xxx>
2016-07-17 05:46:02 +0530

    Added good layout

--- app/src/main/java/autosms/ankur/com/autosms/MainActivity.java
+++ app/src/main/java/autosms/ankur/com/autosms/MainActivity.java
@@ -4,14 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ConnectivityReceiver.ConnectivityReceiverListener{
+
+    WebView wv ;
+    EditText tv ;
+    Button b ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wv = (WebView) findViewById(R.id.webview) ;
+        tv = (EditText) findViewById(R.id.number) ;
+        b = (Button) findViewById(R.id.button) ;
+
+        WebView wv = (WebView) findViewById(R.id.webview);
+        wv.loadUrl("file:///android_res/drawable/hp_logo.gif");
 
         if(checkConnection())
             preparedata();

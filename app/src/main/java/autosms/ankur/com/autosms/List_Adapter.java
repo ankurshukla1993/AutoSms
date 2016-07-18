c871be924f3a3b17bb95b4b0f900a8c00394752d
xxx <xxx>
2016-07-19 05:03:24 +0530

    Almost final code

---
+++ app/src/main/java/autosms/ankur/com/autosms/List_Adapter.java
@@ -0,0 +1,7 @@
+package autosms.ankur.com.autosms;
+
+/**
+ * Created by Ankur on 7/18/2016.
+ */
+public class List_Adapter {
+}
---
+++ app/src/main/java/autosms/ankur/com/autosms/Message_list_adapter.java
@@ -0,0 +1,7 @@
+package autosms.ankur.com.autosms;
+
+/**
+ * Created by Ankur on 7/19/2016.
+ */
+public class Message_list_adapter {
+}
